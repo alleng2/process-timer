2d2f145c772fc42f93edf21eaa1472cc356dc851
Allen Guo <alleng2@illinois.edu>
2023-12-22 14:31:49 -0600

    Added some initial stuff with our names.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstName = "Allen"
+  const secondName = "Stephen"
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world from me!
+          Hello world from {firstName} and {secondName}!
         </p>
         <a
           className="App-link"
@@ -17,6 +20,24 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          <text>
+            Authors:{"\n"}
+            <a
+              className='App-link'
+              href='https://github.com/alleng2'
+              target='_blank'
+              rel='noopener noreferrer'
+            >Allen Guo</a>
+            {' '} and {' '}
+            <a
+              className='App-link'
+              href='https://github.com/stephenmoy14'
+              target='_blank'
+              rel='noopener noreferrer'
+            >Stephen Moy</a>
+          </text>
+        </p>
       </header>
     </div>
   );
