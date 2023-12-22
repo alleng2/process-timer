f7bd5513b11bdf55519253624e3515ecbb23fc53
Allen Guo <alleng2@illinois.edu>
2023-12-22 16:06:59 -0600

    Style edit for app.js.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <p>
           Hello world from {firstName} and {secondName}!
         </p>
-        < Content/ >
+        <Content/>
         <p>
           <text>
             Authors:{"\n"}
