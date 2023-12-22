55210f4ff93704cee12709de06bf5e60e5ad000b
stephenmoy14 <stephenmoy14@gmail.com>
2023-12-22 16:11:08 -0600

    readded reset button

--- my-react-app/src/Content.js
+++ my-react-app/src/Content.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
-
 function Content() {
   // Define state for the counter
   const [counter, setCounter] = useState(0);
-
   // Event handler for the button click
   const handleButtonClick = () => {
     setCounter(counter + 1);
   };
-
+  const handleReset = () => {
+    setCounter(0);
+  };
   return (
     <div>
+      <p>
+        Counter: {counter}
+      </p>
+      <p>
+        <button onClick={handleButtonClick}>Increment Counter</button>
+      </p>
+      <p>
+        <button onClick={handleReset}>Reset Counter</button>
+      </p>
       <p>Counter: {counter}</p>
       <button onClick={handleButtonClick}>Increment Counter</button>
     </div>
   );
 }
-
 export default Content;
